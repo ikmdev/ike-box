54d7a295eb787e0e2eda58a5aae94c4195afe84d
Sikeey dahal <sidahal@deloitte.com>
2025-06-18 10:11:25 -0400

    terraform version,state file storgae

--- domain/dns.tf
+++ domain/dns.tf
@@ -1,4 +1,25 @@
 # DNS setup for testing purposes only.
+
+
+# Configure Terraform
+terraform {
+  required_version = ">= 1.9.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.75.1"
+    }
+  }
+  backend "s3" {
+    region         = "us-east-1"
+    # dynamodb_table = "Fdashield-Infrastructure-TerraformLockStatus-DynamoDB"
+    bucket         = "fdashield-infrastructure-terraformstatefile"
+
+    # Do not change the name below
+    key = "ike-inbox-dnshosting.tfstate"
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
