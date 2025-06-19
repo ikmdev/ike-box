4d6765476e6d2ba64f731b53f50ce8ce5ddca93a
Sikeey dahal <sidahal@deloitte.com>
2025-06-19 19:53:54 -0400

    records update

--- dns-management/subdomain-records/records.tf
+++ dns-management/subdomain-records/records.tf
@@ -1,14 +1,13 @@
 # use existing public hosted zone created during domain registration
 
 data "aws_route53_zone" "public" {
-  name       = main.domain_name
-  depends_on = [aws_route53domains_domain.main]
+  name       = var.domain_name
 }
 
 
 resource "aws_route53_record" "subdomains" {
   for_each = toset(var.subdomains)
-  zone_id  = aws_route53_zone.public.zone_id
+  zone_id  = data.route53_zone.public.zone_id
   name     = "${each.key}.${var.domain_name}"
   type     = "A"
   ttl      = 300
