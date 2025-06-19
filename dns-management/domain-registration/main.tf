887bc76495fdb04b3d406c4eaab5f876225d4404
Sikeey dahal <sidahal@deloitte.com>
2025-06-19 17:11:07 -0400

    version updated

--- dns-management/domain-registration/main.tf
+++ dns-management/domain-registration/main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.85.0"
+      version = ">= 6.0.0"
     }
   }
   backend "s3" {
@@ -29,44 +29,44 @@ resource "aws_route53domains_registered_domain" "main" {
   admin_privacy      = true
   tech_privacy       = true
 
-  # admin_contact {
-  #   address_line_1 = var.contact_info.address_line_1
-  #   city           = var.contact_info.city
-  #   contact_type   = var.contact_info.contact_type
-  #   country_code   = var.contact_info.country_code
-  #   email          = var.contact_info.email
-  #   first_name     = var.contact_info.first_name
-  #   last_name      = var.contact_info.last_name
-  #   phone_number   = var.contact_info.phone_number
-  #   state          = var.contact_info.state
-  #   zip_code       = var.contact_info.zip_code
-  # }
+  admin_contact {
+    address_line_1 = var.contact_info.address_line_1
+    city           = var.contact_info.city
+    contact_type   = var.contact_info.contact_type
+    country_code   = var.contact_info.country_code
+    email          = var.contact_info.email
+    first_name     = var.contact_info.first_name
+    last_name      = var.contact_info.last_name
+    phone_number   = var.contact_info.phone_number
+    state          = var.contact_info.state
+    zip_code       = var.contact_info.zip_code
+  }
 
-  # registrant_contact {
-  #   address_line_1 = var.contact_info.address_line_1
-  #   city           = var.contact_info.city
-  #   contact_type   = var.contact_info.contact_type
-  #   country_code   = var.contact_info.country_code
-  #   email          = var.contact_info.email
-  #   first_name     = var.contact_info.first_name
-  #   last_name      = var.contact_info.last_name
-  #   phone_number   = var.contact_info.phone_number
-  #   state          = var.contact_info.state
-  #   zip_code       = var.contact_info.zip_code
-  # }
+  registrant_contact {
+    address_line_1 = var.contact_info.address_line_1
+    city           = var.contact_info.city
+    contact_type   = var.contact_info.contact_type
+    country_code   = var.contact_info.country_code
+    email          = var.contact_info.email
+    first_name     = var.contact_info.first_name
+    last_name      = var.contact_info.last_name
+    phone_number   = var.contact_info.phone_number
+    state          = var.contact_info.state
+    zip_code       = var.contact_info.zip_code
+  }
 
-  # tech_contact {
-  #   address_line_1 = var.contact_info.address_line_1
-  #   city           = var.contact_info.city
-  #   contact_type   = var.contact_info.contact_type
-  #   country_code   = var.contact_info.country_code
-  #   email          = var.contact_info.email
-  #   first_name     = var.contact_info.first_name
-  #   last_name      = var.contact_info.last_name
-  #   phone_number   = var.contact_info.phone_number
-  #   state          = var.contact_info.state
-  #   zip_code       = var.contact_info.zip_code
-  # }
+  tech_contact {
+    address_line_1 = var.contact_info.address_line_1
+    city           = var.contact_info.city
+    contact_type   = var.contact_info.contact_type
+    country_code   = var.contact_info.country_code
+    email          = var.contact_info.email
+    first_name     = var.contact_info.first_name
+    last_name      = var.contact_info.last_name
+    phone_number   = var.contact_info.phone_number
+    state          = var.contact_info.state
+    zip_code       = var.contact_info.zip_code
+  }
 
 }
 # Reference the VPC
