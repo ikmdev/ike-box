0295507b0d30059f5bc731c4efaf1bf96ad1c5f5
Justin Grant <jlgrock@gmail.com>
2025-07-18 16:35:28 -0400

    mapping new backend in container

--- dns-management/aws-domain-registration/backend/main.tf
+++ dns-management/aws-domain-registration/backend/main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_s3_bucket" "tfstate" {
+resource "aws_s3_bucket_versioning" "tfstate" {
   bucket = var.s3_bucket_name
   acl    = "private"
 
