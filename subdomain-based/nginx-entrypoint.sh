6766ea1e9f887d9b22328ae8f9ffb7ce17ee2a51
Justin Grant <jlgrock@gmail.com>
2025-07-21 16:30:13 -0400

    adding env source in nginx shell script

--- subdomain-based/nginx-entrypoint.sh
+++ subdomain-based/nginx-entrypoint.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
-set -e
+#!/bin/sh
+set -ex
+
+ENV_FILE="/.env"
+
+# Source .env file if it exists
+if [ -f "$ENV_FILE" ]; then
+  set -a
+  # shellcheck source=src/util.sh
+  . "$ENV_FILE"
+  set +a
+else
+  echo ".env file not found at $ENV_FILE"
+fi
 
 # Replace environment variables in the Nginx configuration templates
 envsubst '${BASE_DOMAIN} ${WWW_SUBDOMAIN} ${NEXUS_SUBDOMAIN} ${KOMET_SUBDOMAIN}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
