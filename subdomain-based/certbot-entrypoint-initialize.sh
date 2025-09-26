33bcffcc2d76d33255c53e2e54763b695f433713
s-dahal <sikeey.projects628@gmail.com>
2025-09-26 12:09:06 -0400

    Admin email variablized and minor Readme file update (#25)
    * admin-email for cetificate
    
    * Readme note format
    
    ---------
    
    Co-authored-by: Sikeey dahal <sidahal@deloitte.com>

--- subdomain-based/certbot-entrypoint-initialize.sh
+++ subdomain-based/certbot-entrypoint-initialize.sh
@@ -8,7 +8,7 @@ fi
 
 while :; do
   echo "BASE DOMAIN: ${BASE_DOMAIN}"
-  certbot certonly --email devops@ikm.dev --agree-tos --no-eff-email \
+  certbot certonly --email "${ADMIN_EMAIL}" --agree-tos --no-eff-email \
     -d "${BASE_DOMAIN}" -d "*.${BASE_DOMAIN}" \
     --keep-until-expiring --preferred-challenges dns --manual
   sleep 12h & wait $!
